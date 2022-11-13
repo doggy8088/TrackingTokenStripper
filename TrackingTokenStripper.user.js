e54200c9fc7428d1ec4b2cc70f9b22211d6c7c5f
gershik <31369902+gershik@users.noreply.github.com>
2022-11-13 16:48:16 +0200

    add more tokens, fix links with normal parameters loading infinitely
    also bring the github script in line with https://greasyfork.org/en/scripts/409925-trackingtokenstripper

--- TrackingTokenStripper.user.js
+++ TrackingTokenStripper.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         TrackingTokenStripper
-// @version      1.3
+// @version      1.0
 // @description  Remove most of the annoying tracking token from URL parameters
 // @license      MIT
 // @homepage     https://blog.miniasp.com/
@@ -16,18 +16,8 @@
 (function() {
 
     var s = TrackingTokenStripper(location.href)
-        .remove('fbclid')
-        .remove('utm_source')
-        .remove('utm_medium')
-        .remove('utm_term')
-        .remove('utm_campaign')
-        .remove('utm_content')
-        .remove('utm_cid')
-        .remove('utm_reader')
-        .remove('utm_referrer')
-        .remove('utm_name')
-        .remove('utm_social')
-        .remove('utm_social-type')
+.remove('fbclid')
+        .remove('_openstat')
         .remove('gclid')
         .remove('igshid')
         .remove('_hsenc')
@@ -35,9 +25,13 @@
         .remove('mc_cid')
         .remove('mc_eid')
         .remove('mkt_tok')
+        .remove('utm_source')
+        .remove('utm_medium')
+        .remove('utm_term')
+        .remove('utm_name')
+        .remove('utm_social')
+        .remove('utm_social-type')
         .remove('yclid')
-        .remove('_openstat')
-
         .remove('wt.mc_id')
         .remove('__tn__')
         .remove('gclsrc')
@@ -48,7 +42,6 @@
         .remove('mcd') // sendgrid.com
         .remove('cvosrc') // sendgrid.com
         .remove('cr_cc') // https://blogs.microsoft.com/
-
         .remove('sc_channel')
         .remove('sc_campaign')
         .remove('sc_geo')
@@ -57,32 +50,37 @@
         .remove('trkCampaign')
         .remove('sc_outcome')
         .remove('sc_country')
-
         .remove('__hstc')
         .remove('__hssc')
         .remove('__hsfp')
         .remove('_gl')
-
-        .toString();
-
-    if (s && location.href !== s) {
+        .remove('__s')
+        .remove('dclid')
+        .remove('hsCtaTracking')
+        .remove('ml_subscriber')
+        .remove('ml_subscriber_hash')
+        .remove('msclkid')
+        .remove('oly_anon_id')
+        .remove('oly_enc_id')
+        .remove('rb_clickid')
+        .remove('s_cid')
+        .remove('vero_conv')
+        .remove('vero_id')
+        .remove('fb_source')
+	.toString()
+    if (s && location.href != s) {
         location.href = s;
-        // console.log(s); alert(s);
     }
 
     function TrackingTokenStripper(url) {
         return {
             remove(name) {
-                var [path, ...other] = url.split('?');
-                other = other.join('?');
-
-                var [query, ...hash] = other ? other.split('#') : [query, ''];
-                hash = hash.join('#');
-
+                var [path, other] = url.split('?');
+                var [query, hash] = other ? other.split('#') : [query, ''];
                 if (query) {
                     let new_query = [];
                     for (let param of query.split('&')) {
-                        let [key, val] = param.split('=', 2);
+                        let [key, val] = param.split('=');
                         if (key !== name) {
                             new_query.push(param);
                         }
@@ -92,11 +90,6 @@
 
                 query = query ? query = '?' + query : '';
                 hash = hash ? hash = '#' + hash : '';
-
-                if (url.substr(url.length - 1) == '#') {
-                    hash = '#';
-                }
-
                 return TrackingTokenStripper(path + query + hash);
             },
             toString() {
